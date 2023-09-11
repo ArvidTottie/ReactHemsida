044f3fa23b13ba81cbde68e77f7c0e5622555616
Arvid <arvid.tottie@gmail.com>
2023-09-11 15:37:48 +0200

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Omoss from "./pages/Omoss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import { HashBrowser as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
